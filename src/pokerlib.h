d02b66fa87433dcce9e326d3d5a8a4c7719d8711
Gustavo Henrique <guthhm@gmail.com>
2020-04-22 23:53:23 +0100

    Definição da struct HAND no header

--- src/pokerlib.h
+++ src/pokerlib.h
@@ -1,6 +1,16 @@
 #ifndef POKERLIB_H
 #define POKERLIB_H
 
+typedef struct hand
+{
+    char card_naipes[7];
+    int card_values[7];
+    int best_combination[5];
+    int combination;
+}HAND;
+
 char *divideHands (char cards[10][3], int player);
+
+HAND handIdentifier (HAND player);
 
 #endif
