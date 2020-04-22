b69ac3669eb06f12c454b4a10e96500267be9544
Gustavo Henrique <guthhm@gmail.com>
2020-04-22 13:15:27 +0100

    Alterado o tamanho do array de 10 para 7

--- src/quicklib.c
+++ src/quicklib.c
@@ -4,16 +4,20 @@
 #include <stdbool.h>
 #include <string.h>
 
-int *castCtoI (char array[10][3]) {
+int *castCtoI (char array[7][3]) {
 
-    static int int_array[9];
+    static int int_array[7];
+    
+    /*
+    printf("\nAntes de fazer cast:\n");
 
-    /*for (int i = 0; i < 10; i++)
-        printf("Input: %s\n", array[i]);
+    for (int i = 0; i < 7; i++)
+        printf("%s  ", array[i]);
 
-    printf("\n");*/
+    printf("\n");
+    */
 
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < 7; i++)
     {
         if (array[i][0] <= '9')
             int_array[i] = array[i][0] - '0';
@@ -28,6 +32,16 @@ int *castCtoI (char array[10][3]) {
         if (array[i][0] == 'A')
             int_array[i] = 14;
     }
+    /*
+
+    printf("\nDepois do cast:\n");
+
+    for (int i = 0; i < 7; i++)
+        printf("%d  ", int_array[i]);
+
+    printf("\n\n");
+
+    */
     
     return int_array;
 }
