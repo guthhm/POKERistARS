f2964837636d76830f7bcea3e3a918c16351d86c
Gustavo Henrique <guthhm@gmail.com>
2020-05-20 16:37:47 +0100

    Novas Structs p/ jogadores e cartas (linked lists)

--- src/pokerlib.h
+++ src/pokerlib.h
@@ -6,13 +6,30 @@ typedef struct hand
     char card_naipes[7];
     int card_values[7];
     int best_combination[5];
-    int combination;
+    short combination;
 }HAND;
+
+typedef struct player
+{
+    short check;
+    short fold;
+    short inactive;
+    float score;
+}PLAYER;
+
+typedef struct card_node
+{
+    short card_value;
+    char card_naipe;
+    CARD_NODE *next_card;
+}CARD_NODE;
 
 char *divideHands (char cards[10][3], int player);
 
 void errorCheck (char input[10][3], int input_size);
 
 HAND handIdentifier (HAND player, int hand_size);
+
+PLAYER doContest (HAND player_hand[8], PLAYER contestant[8]);
 
 #endif
